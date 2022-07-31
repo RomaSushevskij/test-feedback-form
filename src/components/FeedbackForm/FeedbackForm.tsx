5146d7ffbf84c1f20c2e78fc50c8ac28b2a147b9
RomaSushevskij <roma.sushevskij@yandex.ru>
2022-07-31 23:33:21 +0300

    fix layout

--- src/components/FeedbackForm/FeedbackForm.tsx
+++ src/components/FeedbackForm/FeedbackForm.tsx
@@ -1,5 +1,6 @@
 import { memo, useState, MouseEvent } from "react";
 
+import paperStyle from "../../common/styles/classes.module.scss";
 import { EMPTY_STRING } from "../../constants";
 import { useAppDispatch, useAppSelector, useField } from "../../hooks";
 import {
@@ -93,96 +94,101 @@ export const FeedbackForm = memo(() => {
   };
 
   return (
-    <div className={style.formWrapper}>
-      <form>
-        <h1>Feedback form</h1>
-        <div className={style.field}>
-          <label htmlFor="Имя Фамилия"> Имя Фамилия</label>
-          <Input
-            error={errorOfFullNameField}
-            value={fullName.fieldValue}
-            onChange={e => fullName.handleFieldValueChange(e, true)}
-            onBlur={fullName.handleFieldValueBlur}
-            type="text"
-            name={FeedbackFormNames.fullName}
-            placeholder="FIRST LAST"
-            id="FullName"
-          />
+    <div className={style.feedbackFormBlock}>
+      <div
+        className={`${style.feedbackFormContainer} ${paperStyle.shadowPaper}`}
+        data-z="paper"
+      >
+        <form className={style.form}>
+          <h1>Feedback form</h1>
+          <div className={style.field}>
+            <label htmlFor="Имя Фамилия"> Имя Фамилия</label>
+            <Input
+              error={errorOfFullNameField}
+              value={fullName.fieldValue}
+              onChange={e => fullName.handleFieldValueChange(e, true)}
+              onBlur={fullName.handleFieldValueBlur}
+              type="text"
+              name={FeedbackFormNames.fullName}
+              placeholder="FIRST LAST"
+              id="FullName"
+            />
+          </div>
+          <div className={style.field}>
+            <label htmlFor="E-mail"> E-mail</label>
+            <Input
+              error={errorOfEmailField}
+              value={email.fieldValue}
+              onChange={email.handleFieldValueChange}
+              onBlur={email.handleFieldValueBlur}
+              type="email"
+              name={FeedbackFormNames.email}
+              placeholder="mail@example.com"
+              id="email"
+            />
+          </div>
+          <div className={style.field}>
+            <label htmlFor="Tel"> Номер телефона</label>
+            <Input
+              error={errorOfPhoneField}
+              value={phone.fieldValue}
+              onInput={phone.onTelInput}
+              onPaste={phone.onTelInputPaste}
+              onKeyDown={phone.onTelInputKeyDown}
+              onBlur={phone.handleFieldValueBlur}
+              type="tel"
+              name={FeedbackFormNames.phone}
+              placeholder="+7 (___) ___-__-__"
+              maxLength={18}
+              id="Tel"
+            />
+          </div>
+          <div className={style.field}>
+            <label htmlFor="birthDate"> Дата рождения</label>
+            <Input
+              error={errorOfBirthDateField}
+              value={birthDate.fieldValue}
+              onChange={birthDate.handleFieldValueChange}
+              onBlur={birthDate.handleFieldValueBlur}
+              type="date"
+              name={FeedbackFormNames.birthDate}
+              id="birthDate"
+            />
+          </div>
+          <div className={style.field}>
+            <label htmlFor="Message"> Сообщение </label>
+            <Textarea
+              value={message.fieldValue}
+              onChange={message.handleFieldValueChange}
+              onBlur={message.handleFieldValueBlur}
+              error={errorOfMessageField}
+              name={FeedbackFormNames.message}
+              placeholder="Сообщение"
+              id="Message"
+            />
+          </div>
+          <div className={style.buttonBlock}>
+            {sendingFormStatus === "loading" ? (
+              <Preloader />
+            ) : (
+              <Button
+                type="submit"
+                className={style.primaryButton}
+                onClick={onButtonSubmitClick}
+                disabled={isButtonDisabled}
+              >
+                Отправить
+              </Button>
+            )}
+          </div>
+        </form>
+        {responseMessage && <SnackBar message={responseMessage} type={snackBarType} />}
+        <div className={style.checkBoxBlock}>
+          {" "}
+          <Checkbox checked={requestStatus} onChangeChecked={setRequestStatus}>
+            Is the request error?
+          </Checkbox>
         </div>
-        <div className={style.field}>
-          <label htmlFor="E-mail"> E-mail</label>
-          <Input
-            error={errorOfEmailField}
-            value={email.fieldValue}
-            onChange={email.handleFieldValueChange}
-            onBlur={email.handleFieldValueBlur}
-            type="email"
-            name={FeedbackFormNames.email}
-            placeholder="mail@example.com"
-            id="email"
-          />
-        </div>
-        <div className={style.field}>
-          <label htmlFor="Tel"> Номер телефона</label>
-          <Input
-            error={errorOfPhoneField}
-            value={phone.fieldValue}
-            onInput={phone.onTelInput}
-            onPaste={phone.onTelInputPaste}
-            onKeyDown={phone.onTelInputKeyDown}
-            onBlur={phone.handleFieldValueBlur}
-            type="tel"
-            name={FeedbackFormNames.phone}
-            placeholder="+7 (___) ___-__-__"
-            maxLength={18}
-            id="Tel"
-          />
-        </div>
-        <div className={style.field}>
-          <label htmlFor="birthDate"> Дата рождения</label>
-          <Input
-            error={errorOfBirthDateField}
-            value={birthDate.fieldValue}
-            onChange={birthDate.handleFieldValueChange}
-            onBlur={birthDate.handleFieldValueBlur}
-            type="date"
-            name={FeedbackFormNames.birthDate}
-            id="birthDate"
-          />
-        </div>
-        <div className={style.field}>
-          <label htmlFor="Message"> Сообщение </label>
-          <Textarea
-            value={message.fieldValue}
-            onChange={message.handleFieldValueChange}
-            onBlur={message.handleFieldValueBlur}
-            error={errorOfMessageField}
-            name={FeedbackFormNames.message}
-            placeholder="Сообщение"
-            id="Message"
-          />
-        </div>
-        <div className={style.buttonBlock}>
-          {sendingFormStatus === "loading" ? (
-            <Preloader />
-          ) : (
-            <Button
-              type="submit"
-              className={style.primaryButton}
-              onClick={onButtonSubmitClick}
-              disabled={isButtonDisabled}
-            >
-              Отправить
-            </Button>
-          )}
-        </div>
-      </form>
-      {responseMessage && <SnackBar message={responseMessage} type={snackBarType} />}
-      <div className={style.checkBoxBlock}>
-        {" "}
-        <Checkbox checked={requestStatus} onChangeChecked={setRequestStatus}>
-          Is the request error?
-        </Checkbox>
       </div>
     </div>
   );
